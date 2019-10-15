2db8da5306c10b699223d2dd7dbf2d5acccb3819
Rafael Tongson <rtongson@peppymiint.local>
2019-10-15 14:27:15 -0700

    food class make

--- Program.cs
+++ Program.cs
@@ -2,6 +2,22 @@
 
 namespace HungryNinja
 {
+    class Food
+    {
+        public string Name;
+        public int Calories;
+        // Foods can be Spicy and/or Sweet
+        public bool IsSpicy; 
+        public bool IsSweet; 
+
+        public Food(string name, int calories, bool isSpicy, bool isSweet)
+        {
+            Name = name;
+            Calories = calories;
+            IsSpicy = isSpicy;
+            IsSweet = isSweet;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
