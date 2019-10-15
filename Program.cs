c72ba6e227c427516c036c7518a599dc1259099f
Rafael Tongson <rtongson@peppymiint.local>
2019-10-15 14:56:18 -0700

    make class ninja and buffet

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HungryNinja
 {
@@ -17,7 +18,69 @@ namespace HungryNinja
             IsSpicy = isSpicy;
             IsSweet = isSweet;
         }
+        // END Food class
     }
+
+    class Buffet
+    {
+        public List<Food> Menu;
+
+        //constructor
+        public Buffet()
+        {
+            Menu = new List<Food>()
+            {
+                new Food("Example Food", 1000, false, false)
+            };
+        }
+
+        //methods:
+        public Food Serve()
+        {
+            Random r = new Random();
+            return Menu[r.Next(0,Menu.Count)];
+        }
+        // END Buffet class
+    }
+    class Ninja
+    {
+        private int calorieIntake;
+        public List<Food> FoodHistory;
+         
+        public Ninja()
+        {
+            calorieIntake = 0;
+            FoodHistory = new List<Food>();
+        }
+         
+        public int CalorieIntake {get;set;}
+        // check if the ninja is full
+        public bool IsFull
+        {
+            get
+            {
+                if(calorieIntake > 1200)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        // eat method
+        public void Eat(Food item)
+        {
+            if(!IsFull)
+            {
+                CalorieIntake+=item.Calories;
+                FoodHistory.Add(item);
+                System.Console.WriteLine($"Ninja ate {item}. Spicy: {item.IsSpicy}, Sweet: {item.IsSweet}.");
+            }
+            else
+            {
+                System.Console.WriteLine("Ninja is full!");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
