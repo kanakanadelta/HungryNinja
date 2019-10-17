e3fcf66e095993dd92fcdd15a0d376d32794e306
Rafael Tongson <rtongson@peppymiint.local>
2019-10-16 17:37:39 -0700

    make interface for consumable

--- Program.cs
+++ Program.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 
 namespace HungryNinja
 {
-    class Food
+    interface IConsumable
     {
-        public string Name;
-        public int Calories;
-        // Foods can be Spicy and/or Sweet
-        public bool IsSpicy; 
-        public bool IsSweet; 
+        string Name {get;set;}
+        int Calories {get;set;}
+        bool IsSpicy {get;set;}
+        bool IsSweet {get;set;}
+        string GetInfo();
+    }   
 
+    class Food : IConsumable
+    {
+        public string Name{get;set;}
+        public int Calories{get;set;}
+        // Foods can be Spicy and/or Sweet
+        public bool IsSpicy{get;set;}
+        public bool IsSweet{get;set;}
+
+        public string GetInfo()
+        {
+            return $"{Name} (Food).  Calories: {Calories}.  Spicy?: {IsSpicy}, Sweet?: {IsSweet}";
+        }
         public Food(string name, int calories, bool isSpicy, bool isSweet)
         {
             Name = name;
